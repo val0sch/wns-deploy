d5d8ef4254bea1489e2dc0dcbb700d296c276312
Valerie <valerie.ielsch@gmail.com>
2023-10-03 10:50:41 +0200

    init

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,15 +6,20 @@ import mongoose from "mongoose";
 import { WilderResolver } from "./resolvers/wilder.resolver";
 
 if (!process.env.SERVER_PORT) {
-  throw new Error("The env variable SERVER_PORT must be defined")
+  throw new Error("The env variable SERVER_PORT must be defined");
 }
 
 (async () => {
-  await mongoose.connect("mongodb://mongodb:27017/wilders", {useUnifiedTopology: true, useNewUrlParser: true});
+  await mongoose.connect("mongodb://mongodb:27017/wilders", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
   const schema = await buildSchema({
     resolvers: [WilderResolver],
   });
-  const server = new ApolloServer({ schema });
+  const server = new ApolloServer({
+    schema,
+  });
 
   server.listen({ port: process.env.SERVER_PORT! }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
